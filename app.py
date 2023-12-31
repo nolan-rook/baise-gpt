dbf0c75138413838aa51cf4ff50edee6a4bb1774
nolan-rook <nolan@baise.nl>
2023-12-31 13:33:08 +0100

    removed function that bot is talking to itself

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ def slack_events():
         handle_message(event)
 
     return '', 200  # HTTP 200 with empty body
-
+# Global variable to keep track of threads the bot has posted in
+bot_threads = set()
 def handle_app_mention(event):
     # Extract the text mentioned to the bot
     prompt_user = event.get('text', '').split('>')[1].strip()
@@ -49,7 +50,7 @@ def handle_app_mention(event):
     slack_client.chat_postMessage(
         channel=event['channel'],
         thread_ts=event['ts'],
-        text=f"Hallo vanaf Baise! :robot_face:nBedankt voor je request, ik ga gelijk voor je aan de slag!"
+        text=f"Hallo vanaf Baise! :robot_face: \nBedankt voor je request, ik ga gelijk voor je aan de slag!"
     )
 
     # Create an OrquestaEndpointRequest object
@@ -69,30 +70,38 @@ def handle_app_mention(event):
         text=result.content
     )
 
+    # Remember the thread_ts to avoid responding to other messages not in the thread
+    bot_threads.add(event['ts'])
+
 def handle_message(event):
-    # Avoid responding to bot's own in a thread
+    # Avoid responding to bot's own messages
     bot_user_id = os.getenv("BOT_ID")
     if event.get('user') == bot_user_id:
         return  # Skip processing if the message is from the bot itself
+
+    # Only respond to messages in a thread
     if event.get('thread_ts'):
-        user_message = event['text']
+        # Check if the bot has already posted in the thread
+        thread_ts = event['thread_ts']
+        if thread_ts in bot_threads:
+            user_message = event['text']
 
-        # Create an OrquestaEndpointRequest object with the user's message
-        request = OrquestaEndpointRequest(
-            key="slack-app",
-            metadata={"chain_id": "97db4100789b46bc8ef5bfb5d2869ff8"},
-            variables={"prompt": user_message}
-        )
+            # Create an OrquestaEndpointRequest object with the user's message
+            request = OrquestaEndpointRequest(
+                key="slack-app",
+                metadata={"chain_id": "97db4100789b46bc8ef5bfb5d2869ff8"},
+                variables={"prompt": user_message}
+            )
 
-        # Query the OrquestaClient for a response
-        result = client.endpoints.query(request)
+            # Query the OrquestaClient for a response
+            result = client.endpoints.query(request)
 
-        # Post the response back to the thread
-        slack_client.chat_postMessage(
-            channel=event['channel'],
-            thread_ts=event['thread_ts'],
-            text=result.content
-        )
+            # Post the response back to the thread
+            slack_client.chat_postMessage(
+                channel=event['channel'],
+                thread_ts=thread_ts,
+                text=result.content
+            )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
