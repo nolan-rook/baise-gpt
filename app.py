ee75459e45b0d3fefa15422ca486e17062b8c88a
nolan-rook <nolan@baise.nl>
2024-01-12 14:51:48 +0100

    changed slack app deployment

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def query_orquesta(event, prompt_user, text_content):
     if not text_content:
         # Invoke the Orquesta deployment
         deployment = client.deployments.invoke(
-            key="pierre_fabre-slack-app",
+            key="slack-app",
             context={
                 "doc": False
             },
@@ -142,7 +142,7 @@ def query_orquesta(event, prompt_user, text_content):
     else:
         # Invoke the Orquesta deployment
         deployment = client.deployments.invoke(
-            key="pierre_fabre-slack-app",
+            key="slack-app",
             context={
                 "environments": [],
                 "doc": None
